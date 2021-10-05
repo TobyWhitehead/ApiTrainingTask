7cf9d1dca3a6dc44ce641ad5bd97b8a99004f662
Toby Whitehead <toby.whitehead@QUALITEST.LOCAL>
2021-10-05 14:17:52 +0100

    added serenity test reporting

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -1,10 +1,10 @@
 package stepDefinitions;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/Features", glue={"stepDefinitions"})
 
 public class TestRunner {
